e2211c56ac27e24c7f5b3e650c686f6b608d20b3
AdminChatter <xiangzhenwuyu@gmail.com>
2024-07-24 23:37:43 -0400

    finish the bug in render function and did small fix on the syntax of generateMarkdown

--- README.md
+++ README.md
@@ -24,15 +24,15 @@ test
 
 ## Credits
 
-Made by test <br>
-1. GitHub: [test](https://github.com/test) <br>
+Made by **adminchatter** <br>
+GitHub: https://github.com/adminchatter
 
 If you have any questions, please contact me at test <br>
 
 ## License
 
-This project is licensed under the **AFL-3.0** license. Click the badge below to learn more about this license. <br>
-[Learn more](https://opensource.org/licenses/AFL-3.0)
+This project is licensed under the **[AFL-3.0](https://opensource.org/licenses/AFL-3.0)** license. <br>Click the license above to learn more about this license. <br>
+
 
 ## Badges
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,80 +4,67 @@ function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
       return "![License: MIT](https://img.shields.io/badge/License-MIT-blue)";
-    case ("AFL-3.0"):
+    case "AFL-3.0":
       return "![License: AFL 3.0](https://img.shields.io/badge/License-AFL--3.0-blue)";
-    case ("Apache-2.0"):
+    case "Apache-2.0":
       return "![License: Apache](https://img.shields.io/badge/License-Apache--2.0-blue)";
-    case ("Artistic-2.0"):
+    case "Artistic-2.0":
       return "![License: Artistic](https://img.shields.io/badge/License-Artistic--2.0-blue)";
-    case ("BSL-1.0"):
+    case "BSL-1.0":
       return "![License: BSL 1.0](https://img.shields.io/badge/License-BSL--1.0-blue)";
-    case ("BSD-2-Clause"):
+    case "BSD-2-Clause":
       return "![License: BSD 2-Clause](https://img.shields.io/badge/License-BSD--2--Clause-blue)";
-    case ("BSD-3-Clause"):
+    case "BSD-3-Clause":
       return "![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD--3--Clause-blue)";
-    case (`BSD-3-Clause-Clear`):
-      return "![License: BSD 3-Clause Clear](https://img.shields.io/badge/License-BSD--3--Clause--Clear-blue)";
-    case ("BSD-4-Clause"):
-      return "![License: BSD 4-Clause](https://img.shields.io/badge/License-BSD--4--Clause-blue)";
-    case (`0BSD`):
-      return "![License: BSD Zero-Clause](https://img.shields.io/badge/License-0BSD-blue)";
-    case (`CC`):
+    case "CC":
       return "![License: Creative Commons](https://img.shields.io/badge/License-CC-blue)";
-    case (`CC0-1.0`):
+    case "CC0-1.0":
       return "![License: CC0 1.0](https://img.shields.io/badge/License-CC0--1.0-blue)";
-    case (`CC-BY-4.0`):
-      return "![License: CC BY 4.0](https://img.shields.io/badge/License-CC--BY--4.0-blue)";
-    case (`CC-BY-SA-4.0`):
-      return "![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC--BY--SA--4.0-blue)";
-    case (`WTFPL`):
-      return "![License: WTFPL](https://img.shields.io/badge/License-WTFPL-blue)";
-    case (`ECL-2.0`):
+    case "ECL-2.0":
       return "![License: ECL 2.0](https://img.shields.io/badge/License-ECL--2.0-blue)";
-    case (`EPL-1.0`):
+    case "EPL-1.0":
       return "![License: EPL 1.0](https://img.shields.io/badge/License-EPL--1.0-blue)";
-    case (`EPL-2.0`):
+    case "EPL-2.0":
       return "![License: EPL 2.0](https://img.shields.io/badge/License-EPL--2.0-blue)";
-    case (`EUPL-1.1`):
+    case "EUPL-1.1":
       return "![License: EUPL 1.1](https://img.shields.io/badge/License-EUPL--1.1-blue)";
-    case (`AGPL-3.0`):
-      return "![License: AGPL 3.0](https://img.shields.io/badge/License-AGPL--3.0-blue)";
-    case (`GPL`):
-      return "![License: GPL](https://img.shields.io/badge/License-GPL-blue)";
-    case (`GPL-2.0`):
+    case "GPL-2.0":
       return "![License: GPL 2.0](https://img.shields.io/badge/License-GPL--2.0-blue)";
-    case (`GPL-3.0`):
+    case "GPL-3.0":
       return "![License: GPL 3.0](https://img.shields.io/badge/License-GPL--3.0-blue)";
-    case (`LGPL`):
-      return "![License: LGPL](https://img.shields.io/badge/License-LGPL-blue)";
-    case (`LGPL-2.1`):
+    case "LGPL-2.1":
       return "![License: LGPL 2.1](https://img.shields.io/badge/License-LGPL--2.1-blue)";
-    case (`LGPL-3.0`):
+    case "LGPL-3.0":
       return "![License: LGPL 3.0](https://img.shields.io/badge/License-LGPL--3.0-blue)";
-    case (`ISC`):
+    case "ISC":
       return "![License: ISC](https://img.shields.io/badge/License-ISC-blue)";
-    case (`LPPL-1.3c`):
+    case "LPPL-1.3c":
       return "![License: LPPL 1.3c](https://img.shields.io/badge/License-LPPL--1.3c-blue)";
-    case (`MS-PL`):
+    case "MS-PL":
       return "![License: MS-PL](https://img.shields.io/badge/License-MS--PL-blue)";
-    case (`MPL-2.0`):
+    case "MS-RL":
+      return "![License: MS-RL](https://img.shields.io/badge/License-MS--RL-blue)";
+    case "MIT":
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-blue)";
+    case "MPL-2.0":
       return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL--2.0-blue)";
-    case (`OSL-3.0`):
-      return "![License: OSL 3.0](https://img.shields.io/badge/License-OSL--3.0-blue)";
-    case (`PostgreSQL`):
-      return "![License: PostgreSQL](https://img.shields.io/badge/License-PostgreSQL-blue)";
-    case (`OFL-1.1`):
-      return "![License: OFL 1.1](https://img.shields.io/badge/License-OFL--1.1-blue)";
-    case (`NCSA`):
+    case "NCSA":
       return "![License: NCSA](https://img.shields.io/badge/License-NCSA-blue)";
-    case (`Unlicense`):
+    case "OFL-1.1":
+      return "![License: OFL 1.1](https://img.shields.io/badge/License-OFL--1.1-blue)";
+    case "OSL-3.0":
+      return "![License: OSL 3.0](https://img.shields.io/badge/License-OSL--3.0-blue)";
+    case "PostgreSQL":
+      return "![License: PostgreSQL](https://img.shields.io/badge/License-PostgreSQL-blue)";
+    case "Unlicense":
       return "![License: Unlicense](https://img.shields.io/badge/License-Unlicense-blue)";
-    case (`Zlib`):
-      return "![License: zlib](https://img.shields.io/badge/License-Zlib-blue)";
+    case "Zlib":
+      return "![License: Zlib](https://img.shields.io/badge/License-Zlib-blue)";
     default:
       return "";
   }
 }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -85,75 +72,59 @@ function renderLicenseLink(license) {
   switch (license) {
     case "MIT":
       return "https://opensource.org/licenses/MIT";
-    case ("AFL-3.0"):
+    case "AFL-3.0":
       return "https://opensource.org/licenses/AFL-3.0";
-    case ("Apache-2.0"):
+    case "Apache-2.0":
       return "https://www.apache.org/licenses/LICENSE-2.0";
-    case ("Artistic-2.0"):
+    case "Artistic-2.0":
       return "https://opensource.org/licenses/Artistic-2.0";
-    case ("BSL-1.0"):
+    case "BSL-1.0":
       return "https://opensource.org/licenses/BSL-1.0";
-    case ("BSD-2-Clause"):
+    case "BSD-2-Clause":
       return "https://opensource.org/licenses/BSD-2-Clause";
-    case ("BSD-3-Clause"):
+    case "BSD-3-Clause":
       return "https://opensource.org/licenses/BSD-3-Clause";
-    case (`BSD-3-Clause-Clear`):
-      return "https://spdx.org/licenses/BSD-3-Clause-Clear.html";
-    case ("BSD-4-Clause"):
-      return "https://spdx.org/licenses/BSD-4-Clause.html";
-    case (`0BSD`):
-      return "https://opensource.org/licenses/0BSD";
-    case (`CC`):
+    case "CC":
       return "https://creativecommons.org/licenses/";
-    case (`CC0-1.0`):
+    case "CC0-1.0":
       return "https://creativecommons.org/publicdomain/zero/1.0/";
-    case (`CC-BY-4.0`):
-      return "https://creativecommons.org/licenses/by/4.0/";
-    case (`CC-BY-SA-4.0`):
-      return "https://creativecommons.org/licenses/by-sa/4.0/";
-    case (`WTFPL`):
-      return "http://www.wtfpl.net/";
-    case (`ECL-2.0`):
+    case "ECL-2.0":
       return "https://opensource.org/licenses/ECL-2.0";
-    case (`EPL-1.0`):
+    case "EPL-1.0":
       return "https://opensource.org/licenses/EPL-1.0";
-    case (`EPL-2.0`):
+    case "EPL-2.0":
       return "https://opensource.org/licenses/EPL-2.0";
-    case (`EUPL-1.1`):
+    case "EUPL-1.1":
       return "https://spdx.org/licenses/EUPL-1.1.html";
-    case (`AGPL-3.0`):
-      return "https://choosealicense.com/licenses/agpl-3.0/";
-    case (`GPL`):
-      return "https://www.gnu.org/licenses/gpl-3.0.html";
-    case (`GPL-2.0`):
+    case "GPL-2.0":
       return "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html";
-    case (`GPL-3.0`):
+    case "GPL-3.0":
       return "https://www.gnu.org/licenses/gpl-3.0.html";
-    case (`LGPL`):
-      return "https://www.gnu.org/licenses/lgpl-3.0.html";
-    case (`LGPL-2.1`):
+    case "LGPL-2.1":
       return "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html";
-    case (`LGPL-3.0`):
+    case "LGPL-3.0":
       return "https://www.gnu.org/licenses/lgpl-3.0.html";
-    case (`ISC`):
+    case "ISC":
       return "https://opensource.org/licenses/ISC";
-    case (`LPPL-1.3c`):
+    case "LPPL-1.3c":
       return "https://www.latex-project.org/lppl/lppl-1-3c/";
-    case (`MS-PL`):
+    case "MS-PL":
       return "https://opensource.org/licenses/MS-PL";
-    case (`MPL-2.0`):
+    case "MS-RL":
+      return "https://opensource.org/licenses/MS-RL";
+    case "MPL-2.0":
       return "https://opensource.org/licenses/MPL-2.0";
-    case (`OSL-3.0`):
-      return "https://opensource.org/licenses/OSL-3.0";
-    case (`PostgreSQL`):
-      return "https://opensource.org/licenses/PostgreSQL";
-    case (`OFL-1.1`):
-      return "https://opensource.org/licenses/OFL-1.1";
-    case (`NCSA`):
+    case "NCSA":
       return "https://spdx.org/licenses/NCSA.html";
-    case (`Unlicense`):
+    case "OFL-1.1":
+      return "https://opensource.org/licenses/OFL-1.1";
+    case "OSL-3.0":
+      return "https://opensource.org/licenses/OSL-3.0";
+    case "PostgreSQL":
+      return "https://opensource.org/licenses/PostgreSQL";
+    case "Unlicense":
       return "https://unlicense.org/";
-    case (`Zlib`):
+    case "Zlib":
       return "https://opensource.org/licenses/Zlib";
     default:
       return "";
@@ -161,12 +132,13 @@ function renderLicenseLink(license) {
 }
 
 
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license) {
-    return `This project is licensed under the **${license}** license. Click the badge below to learn more about this license. <br>
-[Learn more](${renderLicenseLink(license)})`;
+    return `This project is licensed under the **[${license}](${renderLicenseLink(license)})** license. <br>Click the license above to learn more about this license. <br>
+`;
   } else {
     return "";
   }
@@ -200,8 +172,8 @@ ${data.usage}
 
 ## Credits
 
-Made by ${data.username} <br>
-1. GitHub: [${data.username}](https://github.com/${data.username}) <br>
+Made by **${data.username}** <br>
+GitHub: https://github.com/${data.username}
 
 If you have any questions, please contact me at ${data.email} <br>
 
